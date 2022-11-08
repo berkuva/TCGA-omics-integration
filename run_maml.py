2df45a90e6b93ff9cb13fa0330b7d03f32c3f3d3
berkuva <hyunjaecho1213@gmail.com>
2022-11-08 15:14:27 -0500

    Bug fixes and updates

--- run_maml.py
+++ run_maml.py
@@ -1,79 +1,180 @@
-import pandas as pd
+import torch
+import torch.nn as nn
+import numpy as np
+import os
 import random
 from sklearn.preprocessing import StandardScaler
-import tqdm as tqdm
-import json
 from utils import *
 from maml_model import *
+from platform import python_version
+from torch import Tensor
+import scipy.stats
+from numpy.lib.recfunctions import unstructured_to_structured
 
-PATH = ""
+# Python version
+print(python_version())  # '3.7.13'
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+print("Source cancer types: {}".format(source_tasks))
+print("Target cancer types: {}".format(target_tasks))
 
-# torch.manual_seed(2)
-# np.random.seed(3)
-# random.seed(4)
+# Load filtered and imputed transcriptomics, clinical, and proteomics datasets
+PATH = "path/to/directory"
+X_rna = pd.read_csv("{0}rna_imputed*.csv".format(PATH), sep='\t', index_col=0)
+X_cli = pd.read_csv("{0}clinical_imputed*.csv".format(PATH), sep='\t', index_col=0)
+X_pro = pd.read_csv("{0}proteomics_imputed*.csv".format(PATH), sep='\t', index_col=0)
+X_rna.index = X_rna.index.astype(str)
+X_cli.index = X_cli.index.astype(str)
+X_pro.index = X_pro.index.astype(str)
 
-X = pd.read_csv(PATH+"X.csv")
-X_rna = pd.read_csv(PATH+"X_RNA.csv")
-X_cli = pd.read_csv(PATH+"X_clinical.csv")
-X_pro = pd.read_csv(PATH+"X_proteomics.csv")
-censorship = pd.read_csv(PATH+"censorship.csv")['0'].values
-times = pd.read_csv(PATH+"times.csv")['0'].values
-tumor_site = pd.read_csv(PATH+"tumor_site.csv")['0'].values
+# Gather censorship, duration, and cancer type information for survival analysis with cox-loss
+clinical_df = build_clinical_dataframe('path-to-clinical-dataset-directory')
+censorship = get_censorship_from_clinical(clinical_df)
+duration = get_duration_from_clinical(clinical_df)
+tumor_site = get_tumor_site_from_clinical(clinical_df)
 
-with open(PATH + "patient2event_time.txt", 'r') as j:
-    patient2event_time = json.loads(j.read())
+# Integrate omics, contains patients not in source or target cancer types.
+df_cox = pd.concat([X_cli, X_pro, X_rna], axis=1)
+print("Shape of integrated dataset, patients by features: {0}".format(df_cox.shape))
 
-X = torch.cat((torch.FloatTensor(X_rna.values), torch.FloatTensor(X_cli.values)), dim=1)
+# Further remove features with high Pearson correlation
+df_cox = correlation(df_cox, 0.7)
 
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-X = pd.DataFrame(X)
+# Get source data for source cancer types
+target0_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 0)].sample(frac=1)
+target5_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 5)].sample(frac=1)
+target6_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 6)].sample(frac=1)
+target2_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 2)].sample(frac=1)
+target19_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 19)].sample(frac=1)
+target13_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 13)].sample(frac=1)
+target27_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 27)].sample(frac=1)
+target29_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 29)].sample(frac=1)
+target7_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 7)].sample(frac=1)
+target8_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 8)].sample(frac=1)
+target10_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 10)].sample(frac=1)
+target12_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 12)].sample(frac=1)
+target15_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 15)].sample(frac=1)
+target21_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 21)].sample(frac=1)
+target25_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 25)].sample(frac=1)
 
-aggregate_X_corr = correlation(X, 0.7)
-X = X[list(aggregate_X_corr)]
+# Get source data for target cancer types
+target4_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 4)].sample(frac=1)
+target14_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 14)].sample(frac=1)
+target17_df = df_cox.loc[get_pids_from_tumor_id(tumor_site, 17)].sample(frac=1)
 
-X.index = list(patient2event_time.keys())
-X['duration'] = times
-X['censorship'] = censorship
-df_cox = X
-
-source_tasks = get_source_tasks()
-target_tasks = get_target_tasks()
-
-target4_df = df_cox.iloc[np.where(tumor_site == 4)]
-target14_df = df_cox.iloc[np.where(tumor_site == 14)]
-target17_df = df_cox.iloc[np.where(tumor_site == 17)]
-
-target4_df = target4_df.sample(frac=1)
-target14_df = target14_df.sample(frac=1)
-target17_df = target17_df.sample(frac=1)
-
+# Split into support and query, with query set size of 30
 n = 30
-target4_df_test = target4_df.iloc[:n]
-target14_df_test = target14_df.iloc[:n]
-target17_df_test = target17_df.iloc[:n]
+target0_df_query = target0_df.iloc[:n]
+target0_df = target0_df[n:]
+target5_df_query = target5_df.iloc[:n]
+target5_df = target5_df[n:]
+target6_df_query = target6_df.iloc[:n]
+target6_df = target6_df[n:]
+target2_df_query = target2_df.iloc[:n]
+target2_df = target2_df[n:]
+target19_df_query = target19_df.iloc[:n]
+target19_df = target19_df[n:]
+target13_df_query = target13_df.iloc[:n]
+target13_df = target13_df[n:]
+target29_df_query = target29_df.iloc[:n]
+target29_df = target29_df[n:]
+target7_df_query = target7_df.iloc[:n]
+target7_df = target7_df[n:]
+target8_df_query = target8_df.iloc[:n]
+target8_df = target8_df[n:]
+target10_df_query = target10_df.iloc[:n]
+target10_df = target10_df[n:]
+target12_df_query = target12_df.iloc[:n]
+target12_df = target12_df[n:]
+target15_df_query = target15_df.iloc[:n]
+target15_df = target15_df[n:]
+target21_df_query = target21_df.iloc[:n]
+target21_df = target21_df[n:]
+target25_df_query = target25_df.iloc[:n]
+target25_df = target25_df[n:]
+target4_df_query = target4_df.iloc[:n]
+target14_df_query = target14_df.iloc[:n]
+target17_df_query = target17_df.iloc[:n]
 target4_df = target4_df[n:]
 target14_df = target14_df[n:]
 target17_df = target17_df[n:]
 
 
-def get_data(task, test=False):
-    if task == 4 or task == 14 or task == 17:
-        if task == 4:
-            if test:
-                return target4_df_test
-            return target4_df
-        elif task == 14:
-            if test:
-                return target14_df_test
-            return target14_df
-        else:
-            if test:
-                return target17_df_test
-            return target17_df
-    else:
-        index = np.where(tumor_site==task)
-        return df_cox.iloc[index]
+def get_data(task, query=False):
+    """
+    :param task: cancer type.
+    :param query: True if desired data is query set; False if support.
+    :return: data containing desired cancer type and support/query set.
+    """
+    if task == "ACC":
+        if query:
+            return target0_df_query
+        return target0_df
+    elif task == "GBM":
+        if query:
+            return target5_df_query
+        return target5_df
+    elif task == "BRCA":
+        if query:
+            return target6_df_query
+        return target6_df
+    elif task == "LGG":
+        if query:
+            return target2_df_query
+        return target2_df
+    elif task == "UCEC":
+        if query:
+            return target19_df_query
+        return target19_df
+    elif task == "OV":
+        if query:
+            return target13_df_query
+        return target13_df
+    elif task == "UCS":
+        if query:
+            return target29_df_query
+        return target29_df
+    elif task == "LUSC":
+        if query:
+            return target7_df_query
+        return target7_df
+    elif task == "CESC":
+        if query:
+            return target8_df_query
+        return target8_df
+    elif task == "COAD":
+        if query:
+            return target10_df_query
+        return target10_df
+    elif task == "ESCA":
+        if query:
+            return target12_df_query
+        return target12_df
+    elif task == "KIRP":
+        if query:
+            return target15_df_query
+        return target15_df
+    elif task == "PAAD":
+        if query:
+            return target21_df_query
+        return target21_df
+    elif task == "STAD":
+        if query:
+            return target25_df_query
+        return target25_df
+    elif task == "BLCA":
+        if query:
+            return target4_df_query
+        return target4_df
+    elif task == "HNSC":
+        if query:
+            return target14_df_query
+        return target14_df
+    elif task == "LUAD":
+        if query:
+            return target17_df_query
+        return target17_df
 
 
 class MAML():
@@ -96,20 +197,19 @@ class MAML():
         self.tasks_per_meta_batch = tasks_per_meta_batch
 
         # metrics
-        self.print_every = 10
-        self.brier_scores = []
+        self.print_every = 1
         self.concordance_index = []
 
     def get_duration(self, indices):
         return df_cox["duration"].iloc[indices].values
 
-    def inner_loop(self, task, target_query=False, limit=False):
+    def inner_loop(self, task, test_time=False):
         print(task)
         # reset inner model to current maml weights
         temp_weights = [w.clone() for w in self.weights]
 
         # perform training on data sampled from source task support set
-        df_cox_task = get_data(task, test=False)
+        df_cox_task = get_data(task, False)
         df_in = df_cox_task.drop(["duration", "censorship"], axis=1)
         X = torch.FloatTensor(df_in.values).to(device)
 
@@ -119,12 +219,12 @@ class MAML():
                                           theta=theta)
 
             # compute grad and update inner loop weights
-            if target_query == False:
+            if test_time == False:
                 grad = torch.autograd.grad(cox_loss, temp_weights)
                 temp_weights = [w - self.inner_lr * g for w, g in zip(temp_weights, grad)]
 
         # sample new data for meta-update and compute loss
-        df_cox_task_query = get_data(task, test=target_query)
+        df_cox_task_query = get_data(task, True)
 
         duration = df_cox_task_query["duration"].values
 
@@ -133,33 +233,34 @@ class MAML():
         X = torch.FloatTensor(df_in.values).to(device)
 
         theta = self.model.parameterised(X, temp_weights)
-        #         print(theta)
         cox_loss = calculate_cox_loss(df_in=df_cox_task_query,
                                       theta=theta)
 
         c_index = CIndex(theta, duration, df_cox_task_query["censorship"].values)
-        #         c_index = CIndex(duration, theta.detach().numpy(), 1-df_cox_task_query["censorship"].values)
 
-        self.concordance_index.append(c_index)
+        if test_time:
+            self.concordance_index.append(c_index)
 
         return cox_loss
 
     def main_loop_source(self, num_iterations):
         epoch_loss = 0
 
-        for iteration in tqdm.tqdm(range(1, num_iterations + 1)):
+        for iteration in range(1, num_iterations + 1):
             # compute meta loss
             meta_loss = 0
 
             for i in range(self.tasks_per_meta_batch):
                 randnum = random.randint(0, len(source_tasks) - 1)
                 task = source_tasks[randnum]
-                loss = self.inner_loop(task, target_query=False, limit=False)
+                loss = self.inner_loop(task)
 
                 meta_loss += loss
+            print(loss)
 
             # compute meta gradient of loss with respect to maml weights
             meta_grads = torch.autograd.grad(meta_loss, self.weights)
+
             self.model.zero_grad()
             # assign meta gradient to weights and take optimisation step
             for w, g in zip(self.weights, meta_grads):
@@ -174,18 +275,15 @@ class MAML():
                 print("!")
                 epoch_loss = 0
 
-    def main_loop_target(self, num_iterations, test_time=False):
+    def main_loop_target(self, num_iterations, target_task, test_time=False):
         self.concordance_index = []
         epoch_loss = 0
 
-        for iteration in tqdm.tqdm(range(1, num_iterations + 1)):
-            # compute meta loss
+        for iteration in range(1, num_iterations + 1):
             meta_loss = 0
 
             for i in range(self.tasks_per_meta_batch):
-                randnum = random.randint(0, len(target_tasks) - 1)
-                task = target_tasks[randnum]
-                loss = self.inner_loop(task, target_query=test_time, limit=False)
+                loss = self.inner_loop(target_task, test_time=True)
                 meta_loss += loss
 
             # compute meta gradient of loss with respect to maml weights
@@ -196,7 +294,7 @@ class MAML():
                 for w, g in zip(self.weights, meta_grads):
                     w.grad = g
 
-            self.meta_optimiser.step()
+                self.meta_optimiser.step()
 
             # log metrics
             epoch_loss += meta_loss.item() / self.tasks_per_meta_batch
@@ -208,68 +306,53 @@ class MAML():
 
                 epoch_loss = 0
 
+
 meta_lr = 1e-4
 inner_lr = 1e-4
-input_dim = df_cox.shape[1]-2
-hidden_dim = input_dim//5
+input_dim = df_cox.shape[1] - 2
+hidden_dim = input_dim // 5
 output_dim = 1
 model = MAMLModel(input_dim, hidden_dim, output_dim).to(device)
 
-import os
-filepath=PATH
-# filepath = PATH+"/maml_cox.pth"
-# filepath = PATH+"maml_cox_full_new_reduced.pth"
-# filepath = PATH+"maml_cox_full_new_reduced_RNA.pth"
-# filepath = PATH+"maml_cox_full_new_reduced_clinical.pth"
-# filepath = PATH+"maml_cox_full_new_reduced_proteomics.pth"
-filepath = PATH+"maml_cox_full_new_reduced_X_rna_clinical.pth"
-# filepath = PATH+"maml_cox_full_new_reduced_X_rna_pro.pth"
-
-print(filepath)
+filepath = PATH
+# filepath = PATH+"/new_maml_cox.pth"
+# filepath = PATH+"/new_maml_cox_proteomics.pth"
+# filepath = PATH+"/new_maml_cox_clinical.pth"
+filepath = PATH + "/new_maml_cox_rna.pth"
+# filepath = PATH+"/new_maml_cox_rna_clinical.pth"
+# filepath = PATH+"/new_maml_cox_rna_pro.pth"
+# filepath = PATH+"/new_maml_cox_clinical_proteomics.pth"
 
 
-
+# Train on source tasks
 if os.path.exists(filepath):
     checkpoint = torch.load(filepath, map_location=torch.device('cpu'))
     model = MAMLModel(input_dim, hidden_dim, output_dim).to(device)
     model.load_state_dict(checkpoint['model_state_dict'])
-#     model.eval()
     maml = MAML(input_dim, model, inner_lr=inner_lr, meta_lr=meta_lr)
     maml.meta_optimiser.load_state_dict(checkpoint['optimizer_state_dict'])
     print("Loaded saved model.")
 else:
     print("No saved model found. Starting training.")
     maml = MAML(input_dim, model, inner_lr=inner_lr, meta_lr=meta_lr)
-    maml.main_loop_source(num_iterations=50)
+    maml.main_loop_source(num_iterations=100)
     torch.save({
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': maml.meta_optimiser.state_dict(),
-            }, filepath)
-    print("Model saved.")
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': maml.meta_optimiser.state_dict(),
+    }, filepath)
 
-
-
-# Target finetune
-# filepath_ = PATH + "{}maml_cox_full_new_reduced.pth".format(target_tasks)
-# filepath_ = PATH + "{}maml_cox_full_new_reduced_RNA.pth".format(target_tasks)
-filepath_ = PATH + "{}maml_cox_full_new_reduced_clinical.pth".format(target_tasks)
-# filepath_ = PATH + "{}maml_cox_full_new_reduced_proteomics.pth".format(target_tasks)
-# filepath_ = PATH + "{}maml_cox_full_new_reduced_X_rna_clinical.pth".format(target_tasks)
-# filepath_ = PATH + "{}maml_cox_full_new_reduced_pro_clinical.pth".format(target_tasks)
-# filepath_ = PATH + "{}maml_cox_full_new_reduced_rna_pro.pth".format(target_tasks)
-
-for i in range(5):
-    model = MAMLModel(input_dim, hidden_dim, output_dim).to(device)
-    maml.main_loop_target(num_iterations=10, test_time=False) # finetune
+# Fine-tune and evaluate on target tasks.
+tt = "BLCA"  # HNSC; LUAD
+for i in range(30):
+    print(i)
+    model.train()
+    maml.main_loop_target(num_iterations=1, target_task=tt, test_time=False)  # finetune
     model.eval()
-    print("---")
-    maml.main_loop_target(num_iterations=30, test_time=True) # evaluation
+    maml.main_loop_target(num_iterations=1, target_task=tt, test_time=True)  # evaluation
     torch_compute_confidence_interval(torch.FloatTensor(maml.concordance_index))
 
-
+filepath_ = "path-to-save-model.pth"
 torch.save({
     'model_state_dict': model.state_dict(),
     'optimizer_state_dict': maml.meta_optimiser.state_dict(),
     }, filepath_)
-
-
--- utils.py
+++ utils.py
@@ -2,38 +2,141 @@ import torch
 import numpy as np
 from torch import Tensor
 import scipy.stats
+import glob
+import pandas as pd
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+source_tasks = ["ACC", "GBM", "BRCA", "LGG", "UCEC", "OV", "UCS", "LUSC",
+                "CESC", "COAD", "ESCA", "KIRP", "PAAD", "STAD"]
+target_tasks = ["BLCA", "HNSC", "LUAD"]
 
+
+# stackoverflow.com/questions/29294983
 def correlation(dataset, threshold):
+    """
+    :param dataset: A DataFrame.
+    :param threshold: By which features at or above this value of Pearson correlation will be removed.
+    :return: Filtered DataFrame.
+    """
     col_corr = set()
-    corr_matrix = dataset.corr()
+    corr_matrix = dataset.corr().abs()
     for i in range(len(corr_matrix.columns)):
         for j in range(i):
-            if abs(corr_matrix.iloc[i, j]) > threshold:
+            if (corr_matrix.iloc[i, j] >= threshold) and (corr_matrix.columns[j] not in col_corr):
                 colname = corr_matrix.columns[i]
                 col_corr.add(colname)
-    return col_corr
+                if colname in dataset.columns:
+                    del dataset[colname]
+
+    return dataset
 
 
-def get_source_tasks():
-    # 8: CESC; 10: COAD; 12: ESCA; 15: KIRP; 21: PAAD; 25: STAD
-    return [8, 10, 12, 15, 21, 25]
+# Build a dataframe with TCGA clinical data for all cancer types for collecting
+# censorship, duration, and patients' information
+def build_clinical_dataframe(clinical_dir):
+    """
+    :param clinical_dir: path to directory containing the TCGA clinical datasets for cancer types.
+    :return: A merged DataFrame.
+    """
+    clinical = glob.glob('{}/*.txt'.format(clinical_dir))
+
+    clinical_df = pd.DataFrame()
+
+    for i in range(len(clinical)):
+        tumor_type = clinical[i].split("/")[-1].split("-")[0]
+        if tumor_type in source_tasks or tumor_type in target_tasks:
+            df = pd.read_csv(clinical[i], sep='\t', low_memory=False)
+
+            pids = df.columns[1:]
+            pids = [pid.split("-")[-1] for pid in pids]
+
+            columns = df["tcga_participant_barcode"]
+            df = df.T
+            df = df.iloc[1:]
+            df.columns = columns
+            df.index = pids
+            clinical_df = clinical_df.append(df)
+    clinical_df = clinical_df.drop(["Unnamed: 549", "all_p", "all_q"])
+
+    return clinical_df
 
 
-def get_target_tasks():
-    # 4: BLCA; 14: HNSC; 17: LUAD/LUSC
-    #     return [4]
-    #     return [14]
-    return [17]
+def get_censorship_from_clinical(clinical_df):
+    """
+    :param clinical_df: A merged DataFrame containing clinical dataset.
+    :return: censorship data for patients in clinical_df.
+    """
+    censored = clinical_df["CLI_days_to_death"].isna()
+    censorship = censored.replace([False, True], [0, 1])
+    return censorship
 
 
+def get_duration_from_clinical(clinical_df):
+    """
+    :param clinical_df: A merged DataFrame containing clinical dataset.
+    :return: duration for patients in clinical_df.
+    """
+    days_to_death = clinical_df["CLI_days_to_death"].loc[not clinical_df["CLI_days_to_death"].isna()]
+    days_to_last_followup = clinical_df["CLI_days_to_last_followup"].loc[
+        not clinical_df["CLI_days_to_last_followup"].isna()]
+    duration = days_to_death.append(days_to_last_followup)
+    return duration
+
+
+def get_tumor_site_from_clinical(clinical_df):
+    """
+    :param clinical_df: A merged DataFrame containing clinical dataset.
+    :return: tumor_sites for patients in cl inical_df.
+    """
+    return clinical_df["CLI_tumor_tissue_site"]
+
+
+def convert_cancer_types_string_to_int(tumor_site):
+    """
+    :param tumor_site: tumor sites in string
+    :return: tumor sites in int
+    """
+    tumor_site = tumor_site.replace({'lusc': 7,
+                                     'ovary': 13,
+                                     'brain': 5,
+                                     'luad': 17,
+                                     'omentum': 13,
+                                     'colon': 10,
+                                     'kidney': 15,
+                                     'head and neck': 14,
+                                     'stomach': 25,
+                                     'central nervous system': 2,
+                                     'pancreas': 21,
+                                     'breast': 6,
+                                     'endometrial': 19,
+                                     'bladder': 4,
+                                     'cervical': 8,
+                                     'thyroid': 28,
+                                     'esophagus': 12,
+                                     'uterus': 29,
+                                     'adrenal': 0,
+                                     'adrenal gland': 1,
+                                     'extra-adrenal site': 1,
+                                     'testes': 26,
+                                     'bile duct': 3,
+                                     'choroid': 9,
+                                     'thymus': 27,
+                                     'anterior mediastinum': 27})
+    return tumor_site
+
+
+def get_pids_from_tumor_id(tumor_site, tumor_id):
+    return tumor_site[tumor_site["CLI_tumor_tissue_site"] == tumor_id].index
+
+
+# https://github.com/gevaertlab/metalearning_survival
 def calculate_cox_loss(df_in, theta):
     """
-    df_in: dataframe of uncensored patients on whom cox loss is calculated, needed for getting the duration
-    features: patient features from neural network (# patients with tasks, # output_dim)
+    :param df_in: dataframe of uncensored patients on whom cox loss is calculated, needed for getting the duration.
+    :param theta: meta-learning model output.
+    :return: cox hazard loss.
     """
     observed = df_in["censorship"].values
     observed = torch.FloatTensor(observed).to(device)
@@ -55,13 +158,17 @@ def calculate_cox_loss(df_in, theta):
     loss = -(torch.sum(torch.mul(torch.sum(theta - torch.log(torch.sum(torch.mul(exp_theta, R_matrix_batch), dim=1))),
                                  observed)) / len(observed))
 
-    #     print(loss.item())
-
     return loss
 
 
 # https://github.com/gevaertlab/metalearning_survival
 def CIndex(pred, ytime_test, ystatus_test):
+    """
+    :param pred: meta-learning output.
+    :param ytime_test: duration for uncensored patients.
+    :param ystatus_test: patients' censorship.
+    :return: concordance index
+    """
     concord = 0.
     total = 0.
     N_test = ystatus_test.shape[0]
@@ -78,11 +185,16 @@ def CIndex(pred, ytime_test, ystatus_test):
                     elif theta[j] == theta[i]:
                         concord = concord + 0.5
 
-    return (concord / total)
+    return concord / total
 
 
-# https://discuss.pytorch.org/t/what-is-the-proper-way-to-compute-95-confidence-intervals-with-pytorch-for-classification-and-regression/139398/2
+# pytorch discussion 139398
 def torch_compute_confidence_interval(data: Tensor, confidence: float = 0.95):
+    """
+    :param data: tensor data on which confidence intervals will be calculated.
+    :param confidence: confidence level.
+    :return: mean and confidence interval.
+    """
     n = len(data)
     mean: Tensor = data.mean()
     se: Tensor = data.std(unbiased=True) / (n ** 0.5)
